0fd83278e60cbf9d3fdb3224e72e1dc9541ef8ab
Theeraphon Sangduen <biwthrps@Theeraphons-MacBook-Pro.local>
2023-05-22 15:00:04 +0700

    init

---
+++ public/src/js/pushapi.js
@@ -0,0 +1,35 @@
+let enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+if (!window.Promise) {
+    window.Promise = Promise;
+  }
+
+
+function displayConfirmNotification() {
+    let options = {
+        body: "Refno : 202305221452 , จำนวนเงิน 100 บาท",
+        icon: 'src/images/icon_payso.png',
+        image: 'src/images/icon_payso.png',
+    };
+    new Notification("ยืนยันการชำระเงิน", options);
+  }
+  
+  function askForNotificationPermission() {
+    Notification.requestPermission(function (result) {
+      console.log("User Choice", result);
+      if (result !== "granted") {
+        console.log("No notification permission granted!");
+      } else {
+        displayConfirmNotification();
+      }
+    });
+  }
+  
+  if ("Notification" in window) {
+    for (var i = 0; i < enableNotificationsButtons.length; i++) {
+      enableNotificationsButtons[i].style.display = "inline-block";
+      enableNotificationsButtons[i].addEventListener(
+        "click",
+        askForNotificationPermission
+      );
+    }
+  }
